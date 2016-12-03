9c7c099c244d16e7a142e16bb0bf31ccc8acc066
valentin.dubrovskiy <v.dubrovskiy@rambler-co.ru>
2016-12-03 18:49:15 +0300

    add use to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import * as validators from './validators';
 import * as utils from './utils';
 import validation from './validation';
+import use from './use';
 
 export {
     validators,
     utils,
-    validation
+    validation,
+    use
 };
